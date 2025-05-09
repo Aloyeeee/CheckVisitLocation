fbd9136f305495ee882aff6ccd42b346e448a884
Aloyeee <106309362+Aloyeeee@users.noreply.github.com>
2025-05-09 12:04:03 +0300

    Update Dump_CheckVisitLocation.sql

--- Dump_CheckVisitLocation.sql
+++ Dump_CheckVisitLocation.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `checkvisitlocation` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `checkvisitlocation`;
 -- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: checkvisitlocation
@@ -196,4 +198,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-09 11:58:08
+-- Dump completed on 2025-05-09 12:02:20
